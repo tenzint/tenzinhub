515eb4321d12f2ff146dc3470cb3f242ada1b4bd
Tenzin Thabkhae <tenzin.thabkhae@mail.utoronto.ca>
2020-09-12 22:01:38 -0400

    why didn't it commit

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -9,6 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 app.use(logger('dev'));
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
