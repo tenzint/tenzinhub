e8f70142b1683f63aab4c8a8916c0caadbc090d4
Tenzin Thabkhae <tenzin.thabkhae@mail.utoronto.ca>
2020-09-16 12:21:08 -0400

    Tested routes API (not the prediction part). First half of TTC API is ready for Client

--- server/controllers/ttc.controller.js
+++ server/controllers/ttc.controller.js
@@ -1,19 +1,14 @@
 var xml2js = require('xml2js');
+const got = require('got');
 
-exports.getTtcRoutes = (req, res) => {
-  const got = require('got');
-
+// Get a list of all Bus/Streetcar routes. eg. 76, Royal York Road bus...
+exports.getTtcAllRoutes = (req, res) => {
   got('http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=ttc')
     .then(response => {
-
-      console.log(response.body);
-
-      xml2js.parseStringPromise(response.body /*, options */).then(function (result) {
-        console.dir(result);
-        console.log('Done');
-
-        return res.send({ message: result.body.route });
-      })
+      xml2js.parseStringPromise(response.body /*, options */)
+        .then(function (result) {
+          return res.send({ message: result.body.route });
+        })
         .catch(function (err) {
           // Failed
           return res.status(400).send({ error: err });
@@ -26,5 +21,31 @@ exports.getTtcRoutes = (req, res) => {
       return res.status(400).send({ error: error });
     });
 }
-exports.postTtcSpecific = (req, res) => {
+
+// Get the detailed route of a specific bus/streetcar given its ID number
+// Param: req.body.routeTag
+exports.postTtcSpecificRoute = (req, res) => {
+  let routeTag = req.body.routeTag;
+  if (!routeTag) {
+    routeTag = '501';
+    console.log('Client did not set the route tag of TTC bus - defaulting to 501');
+  }
+  got(`http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=ttc&r=${routeTag}`)
+    .then(response => {
+      xml2js.parseStringPromise(response.body /*, options */)
+        .then(function (result) {
+          // console logging the unparsed XML data takes dozens of seconds to just print. Too long!
+          return res.send({ message: result });
+        })
+        .catch(function (err) {
+          // Failed
+          return res.status(400).send({ error: err });
+        });
+
+    })
+    .catch(error => {
+      console.log(error.response.body);
+      //=> 'Internal server error ...'
+      return res.status(400).send({ error: error });
+    });
 }
--- server/routes/ttc.routes.js
+++ server/routes/ttc.routes.js
@@ -1,6 +1,6 @@
 const ttcController = require("../controllers/ttc.controller");
 
 module.exports = function(app) {
-  app.get("/api/ttc/routes", ttcController.getTtcRoutes);
-  app.post("/api/ttc/nvas", ttcController.postTtcSpecific);
+  app.get("/api/ttc/routes/all", ttcController.getTtcAllRoutes);
+  app.post("/api/ttc/routes/specific", ttcController.postTtcSpecificRoute);
 }
