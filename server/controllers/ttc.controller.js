730ac00f5063834eec3ecb4cc356ef6531f88149
Tenzin Thabkhae <tenzin.thabkhae@mail.utoronto.ca>
2020-09-16 11:52:35 -0400

    changed the async await syntax to .then-.catch syntax.

--- server/controllers/ttc.controller.js
+++ server/controllers/ttc.controller.js
@@ -3,9 +3,9 @@ var xml2js = require('xml2js');
 exports.getTtcRoutes = (req, res) => {
   const got = require('got');
 
-  (async () => {
-    try {
-      const response = await got('http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=ttc');
+  got('http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=ttc')
+    .then(response => {
+
       console.log(response.body);
 
       xml2js.parseStringPromise(response.body /*, options */).then(function (result) {
@@ -18,12 +18,13 @@ exports.getTtcRoutes = (req, res) => {
           // Failed
           return res.status(400).send({ error: err });
         });
-    } catch (error) {
+
+    })
+    .catch(error => {
       console.log(error.response.body);
       //=> 'Internal server error ...'
       return res.status(400).send({ error: error });
-    }
-  })();
+    });
 }
 exports.postTtcSpecific = (req, res) => {
 }
